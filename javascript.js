2fa897125d9e5dec722ee5a30b50634ff3f61af4
Diego <diegoyegrosa@gmail.com>
2022-12-25 00:43:22 -0300

    Problem solved  using JQuery

--- javascript.js
+++ javascript.js
@@ -1,24 +1,24 @@
 var text = document.querySelector("#numeroDocumento");
-
 var ruc = "";
 var numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var guionExiste =false
 var longitud = 0;
 
-text.addEventListener('keyup', () => {
+function validarNumeroDocumento() {
     longitud = ruc.length
-    if (text.value.length < longitud){
-        if (!ruc.includes('-'))
-            guionExiste = false
+    if (!ruc.includes('-'))
+        guionExiste = false
+    if (text.value.length < longitud) {
         ruc = text.value;
         return;
     }
-    if (guionExiste && ruc.charAt(longitud - 1)!== "-") {
+    if (guionExiste && ruc.charAt(longitud - 1) !== "-") {
         text.value = ruc;
         return;
     }
-
-    ingresado = text.value.charAt(text.value.length - 1);
+    
+    let ingresado = text.value.charAt(text.value.length - 1);
+    
     if (text.value.length === 1 && ingresado === "-") {
         text.value = '';
         return;
@@ -27,18 +27,17 @@ text.addEventListener('keyup', () => {
         text.value = ruc;
         return;
     }
-
-    if (ingresado === '-') {
-            guionExiste = true;
-    }
-
-
     
+    if (ingresado === '-') {
+        guionExiste = true;
+    }
     if (numeros.includes(ingresado) || ingresado === '-') {
-         ruc += ingresado;
-            text.value = ruc;  
+        ruc += ingresado;
+        text.value = ruc;
     } else {
         text.value = ruc;
     }
-            
+}
+$("#numeroDocumento").keyup(function (e) {
+    validarNumeroDocumento();
 })
