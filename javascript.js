18162cbaa3051a72a80e4b5651ab5d0f2122026d
Diego <diegoyegrosa@gmail.com>
2022-12-25 21:58:13 -0300

    tested queryselector jquery

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var text = document.querySelector("#numeroDocumento");
+var text = document.querySelector("#numeroDocumento")
 var ruc = "";
 var numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var guionExiste =false
